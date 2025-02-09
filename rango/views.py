2ceb86d6bfa3f978f34b04874147b1183714d514
“Sen <2961134y@student.gla.ac.uk>
2025-02-09 16:36:30 +0000

    chapter 6 exercise

--- rango/views.py
+++ rango/views.py
@@ -9,12 +9,14 @@ def index(request):
     # Retrieve the top 5 only -- or all if less than 5.
     # Place the list in our context_dict dictionary (with our boldmessage!) # that will be passed to the template engine.   
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     context_dict = {}
     context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
